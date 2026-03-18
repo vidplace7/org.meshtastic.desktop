fa15f40ec9a358e5cc44870b7d2f54ee08ddbf6f
Austin Lane <vidplace7@gmail.com>
2026-03-17 23:08:22 -0400

    Set TMP_DIR

--- meshtastic-wrapper.sh
+++ meshtastic-wrapper.sh
@@ -4,8 +4,13 @@ export JAVA_HOME=/app/jre
 # Create config directory if it doesn't exist
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/meshtastic-desktop"
 
+# Use a writable tmpdir within the Flatpak sandbox
+export TMPDIR="${XDG_CACHE_HOME:-$HOME/.cache}/tmp"
+mkdir -p "$TMPDIR"
+
 # Set Java preferences location to use Flatpak's config directory
 exec /app/jre/bin/java \
+    -Djava.io.tmpdir="$TMPDIR" \
     -Djava.awt.headless=false \
     -Dawt.useSystemAAFontSettings=on \
     -Dswing.aatext=true \
